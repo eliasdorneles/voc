d440a7b92d9c5a631a91527f6ba2cb83598e9c3e
Elias Dorneles <eliasdorneles@gmail.com>
2017-03-13 18:21:23 -0400

    add test for extending java class

---
+++ tests/test_interoperability.py
@@ -0,0 +1,56 @@
+from .utils import TranspileTestCase
+
+
+class InteroperabilityTest(TranspileTestCase):
+    def test_extend_java_class_with_simple_constructor(self):
+        self.assertJavaExecution(
+            """
+            class MyPythonClass(extends=com.example.MyClass):
+                pass
+
+            MyPythonClass()
+            print("Done.")
+            """,
+            java={
+                'com/example/MyClass': """
+                package com.example;
+
+                public class MyClass {
+                    public MyClass() {
+                        System.out.println("MyClass started");
+                    }
+                }
+                """
+            },
+            out="""
+            MyClass started
+            Done.
+            """,
+        )
+
+    def test_extend_java_class_with_constructor_that_needs_args(self):
+        self.assertJavaExecution(
+            """
+            class MyPythonClass(extends=com.example.MyClass):
+                def __init__(self, x: java.lang.Integer) -> void:
+                    print('x', x)
+
+            MyPythonClass()
+            print("Done.")
+            """,
+            java={
+                'com/example/MyClass': """
+                package com.example;
+
+                public class MyClass {
+                    public MyClass(Integer x) {
+                        System.out.println("MyClass started with " + x);
+                    }
+                }
+                """
+            },
+            out="""
+            MyClass started with 1
+            Done.
+            """,
+        )
